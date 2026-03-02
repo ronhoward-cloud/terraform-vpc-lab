89440f529b7015c22183d5bd5971a5132306c2e0
Ron Howard II <ronehowardii@gmail.com>
2026-03-02 10:56:05 -0500

    Refactor: modular VPC with dev environment

---
+++ envs/dev/main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  region              = "us-east-1"
+  az                  = "us-east-1a"
+  vpc_cidr            = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.1.0/24"
+  private_subnet_cidr = "10.0.2.0/24"
+}
