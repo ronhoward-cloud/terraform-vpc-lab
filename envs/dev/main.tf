8185a9531a0f09c913d7c6dca708d15ac7d6fde3
Ron Howard II <ronehowardii@gmail.com>
2026-03-04 10:18:12 -0500

    Add remote state backend: S3 bucket, DynamoDB lock, dev backend config

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "ronhoward-terraform-state"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
 module "vpc" {
   source = "../../modules/vpc"
 
